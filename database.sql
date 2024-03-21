bcc3d9662496d21b8f8623032824caeaf72da280
Thigos <dev.findviewbyid@gmail.com>
2024-03-20 21:40:33 -0300

    20_03 Atualiza~

--- database.sql
+++ database.sql
@@ -5,30 +5,6 @@ CREATE DATABASE IF NOT EXISTS 9Solutions_db;
 
 USE 9Solutions_db;
 
--- Donation Layer
-
-CREATE TABLE IF NOT EXISTS tbCaixa(
-    idCaixa INT PRIMARY KEY AUTO_INCREMENT
-    ,genero CHAR(1) CHECK (genero IN ('M', 'F')) NOT NULL
-    ,rangeIdade CHAR(3) NOT NULL #EX 0-0
-    ,carta TEXT
-    ,foto VARCHAR(255) #URL
-);
-
-CREATE TABLE IF NOT EXISTS tbItem(
-    idItem INT PRIMARY KEY AUTO_INCREMENT
-    ,itemNome VARCHAR(100)
-    ,valor DECIMAL(5, 2)
-);
-
-CREATE TABLE IF NOT EXISTS tbItemCaixa(
-    idItemCaixa INT PRIMARY KEY AUTO_INCREMENT
-    ,fkCaixa INT
-    ,fkItem INT
-    ,CONSTRAINT fk_tbItemCaixa_tbCaixa Foreign Key (fkCaixa) REFERENCES tbCaixa(idCaixa)
-    ,CONSTRAINT fk_tbItemCaixa_tbItem Foreign Key (fkItem) REFERENCES tbItem(idItem)
-);
-
 -- User Layer
 
 CREATE TABLE IF NOT EXISTS tbConhecimentoProjeto(
@@ -47,14 +23,39 @@ CREATE TABLE IF NOT EXISTS tbDoador(
     idDoador INT PRIMARY KEY AUTO_INCREMENT
     ,nomeCompleto VARCHAR(60) NOT NULL
     ,isPF BOOLEAN NOT NULL
-    ,cpf CHAR(14) #EX: xxx.xxx.xxx-xx
+    ,identificadorPessoa CHAR(14) #EX: xxx.xxx.xxx-xx
     ,telefone CHAR(15) #EX: (00) 00000-0000
     ,email VARCHAR(100)
     ,dtCadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     ,fkConhecimentoProjeto INT
-    ,fkCaixa INT
     ,CONSTRAINT fk_tbDoador_tbConhecimentoProjeto Foreign Key (fkConhecimentoProjeto) REFERENCES tbConhecimentoProjeto(idConhecimentoProjeto)
-    ,CONSTRAINT fk_tbDoador_tbCaixa Foreign Key (fkCaixa) REFERENCES tbCaixa(idCaixa)
+);
+
+
+-- Donation Layer
+
+CREATE TABLE IF NOT EXISTS tbCaixa(
+    idCaixa INT PRIMARY KEY AUTO_INCREMENT
+    ,genero CHAR(1) CHECK (genero IN ('M', 'F')) NOT NULL
+    ,rangeIdade CHAR(3) NOT NULL #EX 0-0
+    ,carta TEXT
+    ,foto VARCHAR(255) #URL
+    ,fkDoador INT
+    ,CONSTRAINT fk_tbCaixa_tbDoador Foreign Key (fkDoador) REFERENCES tbDoador(idDoador)
+);
+
+CREATE TABLE IF NOT EXISTS tbItem(
+    idItem INT PRIMARY KEY AUTO_INCREMENT
+    ,itemNome VARCHAR(100)
+    ,valor DECIMAL(5, 2)
+);
+
+CREATE TABLE IF NOT EXISTS tbItemCaixa(
+    idItemCaixa INT PRIMARY KEY AUTO_INCREMENT
+    ,fkCaixa INT
+    ,fkItem INT
+    ,CONSTRAINT fk_tbItemCaixa_tbCaixa Foreign Key (fkCaixa) REFERENCES tbCaixa(idCaixa)
+    ,CONSTRAINT fk_tbItemCaixa_tbItem Foreign Key (fkItem) REFERENCES tbItem(idItem)
 );
 
 -- Metrics Layer
